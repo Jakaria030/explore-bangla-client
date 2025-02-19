15c76540a8c2b576c8d5673701909f01dfb795fe
Jakaria030 <gulamjakaria99@gmail.com>
2025-02-19 11:34:07 +0600

    Sorted by price functionality added in the trips page

--- src/pages/AllTrips/AllTrips.jsx
+++ src/pages/AllTrips/AllTrips.jsx
@@ -1,9 +1,25 @@
+import { useState } from "react";
 import useAllPackages from "../../hooks/useAllPackages";
 import PackageCard from "../components/PackageCard";
 import SectionTitle from "../components/SectionTitle";
 
 const AllTrips = () => {
     const { packages, isAllPackagesLoading } = useAllPackages();
+    const [sortOrder, setSortOrder] = useState("");
+
+    const handleSortChange = (e) => {
+        setSortOrder(e.target.value);
+    };
+
+    const sortedPackages = [...packages].sort((a, b) => {
+        if (sortOrder === "ascending") {
+            return a.price - b.price;
+        } else if (sortOrder === "descending") {
+            return b.price - a.price;
+        }
+
+        return 0;
+    });
 
     return (
         <section className="max-w-8xl mx-auto px-5 mb-8 md:mb-16">
@@ -11,13 +27,21 @@ const AllTrips = () => {
                 <SectionTitle title={"Our all packages"}></SectionTitle>
             </div>
 
+            <div className="flex items-center justify-end py-4">
+                <select onChange={handleSortChange} defaultValue="Sort By Price" className="select select-bordered w-full max-w-xs">
+                    <option disabled>Sort By Price</option>
+                    <option value="ascending">Ascending</option>
+                    <option value="descending">Descending</option>
+                </select>
+            </div>
+
             {
-                !isAllPackagesLoading && packages.length > 0 ?
-                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5">
-                    {
-                        packages.map(item => <PackageCard key={item._id} item={item}></PackageCard>)
-                    }
-                </div> : <div className="py-4 bg-teal/50 text-lg font-bold text-center text-black">No Package Found!</div>
+                !isAllPackagesLoading && sortedPackages.length > 0 ?
+                    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5">
+                        {
+                            sortedPackages.map(item => <PackageCard key={item._id} item={item}></PackageCard>)
+                        }
+                    </div> : <div className="py-4 bg-teal/50 text-lg font-bold text-center text-black">No Package Found!</div>
             }
         </section>
     );
